95609968aa8710efbcc52b5080f2eb9551e6425f
rusty-peilin <cqdxcs3@163.com>
2023-09-19 01:00:03 +0800

    done heads up display

--- 01dodge/main.gd
+++ 01dodge/main.gd
@@ -7,12 +7,16 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
@@ -20,6 +24,7 @@ func _on_start_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
@@ -49,4 +54,6 @@ func _on_mob_timer_timeout():
 	add_child(mob)
 	
 func _ready():
-	new_game()
+	pass
+	
+
