aeda8abfc13841ad17a0d48429e998573121f72c
rusty-peilin <cqdxcs3@163.com>
2023-09-27 10:06:22 +0800

    17 blink when hurt

--- 03makertech_zelda/player/player.gd
+++ 03makertech_zelda/player/player.gd
@@ -6,12 +6,18 @@ signal healthChanged
 
 @export var speed: int = 95
 @onready var animations = $AnimationPlayer
-@onready var hurtColor = $Sprite2D/ColorRect
+@onready var effects = $Effects
+@onready var hurtTimer = $hurtTimer
+
+#@onready var hurtColor = $Sprite2D/ColorRect
 
 @export var maxHealth = 3
 @onready var currentHealth: int = maxHealth
 
 @export var knockbackPower: int = 500
+
+func _ready():
+	effects.play("RESET")
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
@@ -50,6 +56,10 @@ func _on_hurt_box_area_entered(area):
 			
 		healthChanged.emit(currentHealth)
 		knockback(area.get_parent().velocity)
+		effects.play("hurtBlink")
+		hurtTimer.start()
+		await hurtTimer.timeout
+		effects.play("RESET")
 
 func knockback(enemyVelocity: Vector2):
 	var knockbackDirection = (enemyVelocity - velocity).normalized()*knockbackPower
