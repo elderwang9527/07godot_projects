b3f01fe56bde6acf813d14859b3872a385921cb7
rusty-peilin <cqdxcs3@163.com>
2023-10-02 10:20:26 +0800

    23 stack items

--- 03makertech_zelda/inventory/inventory.gd
+++ 03makertech_zelda/inventory/inventory.gd
@@ -4,12 +4,35 @@ class_name  Inventory
 
 signal updated
 
-@export var items: Array[InventoryItem]
+@export var slots: Array[InventorySlot]
 
-func insert(item: InventoryItem):	
-	for i in range(items.size()):
-		if !items[i]:
-			items[i] = item
-			break		
-	
+## 23-0539前
+#func insert(item: InventoryItem):
+#	for slot in slots:
+#		if slot.item == item:
+#			slot.amount += 1
+#			updated.emit()
+#			return
+#
+#	for i in range(slots.size()):
+#		if !slots[i].item:
+#			slots[i].item = item
+#			slots[i].amount = 1
+#			updated.emit()
+#			return
+
+# 23-0539后，视频没解释这段代码，需要自己研究
+func insert(item: InventoryItem):
+#	var itemSlots = slots.filter(func(slot): return slot.item == item && slot.amount < slot.item.max)
+	var itemSlots = slots.filter(func(slot): return slot.item == item)
+	if !itemSlots.is_empty():
+		itemSlots[0].amount += 1
+	else:
+		var emptySlots = slots.filter(func(slot): return slot.item == null)
+		if !emptySlots.is_empty():
+			emptySlots[0].item = item
+			emptySlots[0].amount = 1
+
 	updated.emit()
+			
+		
